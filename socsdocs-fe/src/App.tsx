a5a2cd5e04383ef4c79070334ba0c40ecd792397
Anthony <33004321+anthony1x6000@users.noreply.github.com>
2026-04-25 03:41:30 +0000

    feat: use side settings bar at bottom

--- socsdocs-fe/src/App.tsx
+++ socsdocs-fe/src/App.tsx
@@ -2,24 +2,28 @@ import useDopamineStore from './store/useDopamineStore';
 
 import { Shadertoy } from 'react-shadertoy';
 
-import {PageTitle} from './components/Title';
-import {Button} from './components/ButtonPrimary';
-import {Slider} from './components/DopamineSlider';
+import {PageTitle} from './components/ui/Title';
+import {Button} from './components/ui/ButtonPrimary';
+import {Slider} from './components/ui/DopamineSlider';
+
+import { BackgroundMusic } from './components/BackgroundMusic';
 
 import './App.css';
+import { SettingsBar } from './components/ui/SettingsBar';
 
 
 function App() {
   const level = useDopamineStore((state) => state.level);
   
   return (
-    <>
+    <div className="relative h-screen w-screen overflow-hidden">
+      <BackgroundMusic />
       <Shadertoy className='hero-background' fragmentShader="back.glsl" style={{ width: '100%', height: '100%' }} />
       <PageTitle text="cool title" level={level} />
       <Button text="Button click me!" onClick={() => console.log("Button clicked!")} />
 
-      <Slider />
-    </>
+      <SettingsBar />
+    </div>
   )
 }
 
