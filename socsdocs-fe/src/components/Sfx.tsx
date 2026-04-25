c3b937307da52b2126e6aad14d3564b447680204
Anthony <33004321+anthony1x6000@users.noreply.github.com>
2026-04-25 01:09:11 +0000

    move sfx to public

--- socsdocs-fe/src/components/Sfx.tsx
+++ socsdocs-fe/src/components/Sfx.tsx
@@ -1,4 +1,4 @@
-import bongSound from '/src/assets/sfx/bong.mp3';
-import bongFinish from '/src/assets/sfx/bong-finish.mp3';
+import bongSound from '/public/sfx/bong.mp3';
+import bongFinish from '/public/sfx/bong-finish.mp3';
 
 export { bongSound, bongFinish };
