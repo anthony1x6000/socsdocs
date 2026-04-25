378467aff3a348d05ac4b2f0ba92b918a77e440b
Anthony <33004321+anthony1x6000@users.noreply.github.com>
2026-04-25 00:43:01 +0000

    Button, use tailwind merge

--- socsdocs-fe/src/components/ButtonPrimary.tsx
+++ socsdocs-fe/src/components/ButtonPrimary.tsx
@@ -1,11 +1,22 @@
+import { twMerge } from "tailwind-merge";
+
 interface ButtonProps {
     text: string;
-    onClick: () => void;  
+    onClick: () => void; 
+    className?: string;
 }
 
-export function Button({ text, onClick }: ButtonProps) {
+export function Button({ text, onClick, className }: ButtonProps) {
     return (
-        <button onClick={onClick}>
+        <button 
+            onClick={onClick}
+            className={twMerge(
+                // default styles 
+                "px-4 py-2 bg-blue-500 text-white font-semibold rounded hover:bg-blue-600 transition-colors",
+                // extra styles down here 
+                className
+            )}
+        >
             {text}
         </button>
     );
