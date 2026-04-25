03be8bc85eec92448cbf8f4f94f45bc333d91622
Anthony <33004321+anthony1x6000@users.noreply.github.com>
2026-04-25 01:49:09 +0000

    add baseStyle const for twMerge

--- socsdocs-fe/src/components/ui/Headers.tsx
+++ socsdocs-fe/src/components/ui/Headers.tsx
@@ -1,16 +1,20 @@
 import useDopamineStore from "../../store/useDopamineStore";
 import { titleLevelStyle, titleLevelAnimation } from './dopamineLevelStyles';
 import { motion } from 'framer-motion';
+import { twMerge } from 'tailwind-merge';
 
 interface HeaderProps {
   text: string;
+  className?: string;
 }
 
-export function Header({ text }: HeaderProps) {
+const baseStyle = "";
+
+export function Header({ text, className }: HeaderProps) {
     const level = useDopamineStore((state) => state.level);
     
     return (
-        <h1 className={titleLevelStyle[level]}>
+        <h1 className={twMerge(baseStyle, titleLevelStyle[level], className)}>
             <motion.span className="inline-block" {...titleLevelAnimation[level]}>
                 {text}
             </motion.span>
