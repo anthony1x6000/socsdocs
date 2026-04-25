45ce547fe56191e09c4c13f2083f7ecea2b178fe
Anthony <33004321+anthony1x6000@users.noreply.github.com>
2026-04-25 15:52:19 +0000

    feat: flexbox and subtitle

--- socsdocs-fe/src/App.tsx
+++ socsdocs-fe/src/App.tsx
@@ -6,6 +6,8 @@ import { BackgroundMusic } from './components/BackgroundMusic';
 
 import './App.css';
 import { SettingsBar } from './components/ui/SettingsBar';
+import Subtitle from './components/ui/Subtitle';
+import FlexBox from './components/ui/FlexBox';
 
 
 function App() {
@@ -13,7 +15,14 @@ function App() {
     <div className="relative h-screen w-screen overflow-hidden">
       <BackgroundMusic />
       <Shadertoy className='hero-background' fragmentShader="back.glsl" style={{ width: '100%', height: '100%' }} />
-      <PageTitle />
+      
+      <PageTitle className='mt-[3em]'/>
+      <FlexBox className='gap-[2em]'>
+        <Subtitle text="COME HERE TO STUDY" />
+        <Subtitle text="LOGIN" />
+        <Subtitle text="SIGN UP" />
+      </FlexBox>
+      
 
       <SettingsBar />
     </div>
