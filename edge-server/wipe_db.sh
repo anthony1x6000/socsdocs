d3ced47e7dc4b2456629f656d1b5aca709307ff8
Anthony <33004321+anthony1x6000@users.noreply.github.com>
2026-04-26 14:13:58 +0000

    feat: automate db creation and deletion

--- edge-server/wipe_db.sh
+++ edge-server/wipe_db.sh
@@ -1,5 +1,13 @@
 rm -rf .wrangler/state/v3/d1
 rm -rf drizzle
 
-printf "now run\n"
-printf "pnpm drizzle-kit generate \n and then \n npx wrangler d1 execute socs-db --local --file=./drizzle/0000_faithgul_butterfly.sql\n" 
+npx drizzle-kit generate --name=init
+
+MIGRATION_FILE=$(ls drizzle/*.sql | head -n 1)
+
+if [ -f "$MIGRATION_FILE" ]; then
+  echo "Applying migration ($MIGRATION_FILE) to local D1 database..."
+  npx wrangler d1 execute socs-db --local --file="$MIGRATION_FILE"
+else
+  echo "Error: No migration file found in ./drizzle/"
+fi
