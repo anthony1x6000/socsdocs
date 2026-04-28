61bc7ce5837b49eb18689943d86ec27c1b4f3bf4
Anthony <33004321+anthony1x6000@users.noreply.github.com>
2026-04-28 14:04:30 +0000

    feat: externfileupload component

---
+++ socsdocs-fe/src/stories/ExternFileUpload.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { ExternFileUpload } from '../components/ExternFileUpload';
+
+const meta: Meta<typeof ExternFileUpload> = {
+  title: 'Components/ExternFileUpload',
+  component: ExternFileUpload,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  decorators: [
+    (Story) => (
+      <div className="p-8 bg-black min-h-[400px] flex items-center justify-center">
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
